67e57e5cf30961cc34de88b0ae8a4b31381ce812
sriramede9 <only4programming03@gmail.com>
2020-07-26 19:58:24 -0400

    'initial-setup

---
+++ src/main/java/com/sri/UserRoles/UserRolesApplication.java
@@ -0,0 +1,13 @@
+package com.sri.UserRoles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRolesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRolesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sri/UserRoles/UserRolesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sri.UserRoles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRolesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
